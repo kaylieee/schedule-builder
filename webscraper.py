69f92e5a486d51dccc6d787ce70bf31a13b9a74a
kaylieee <kaylieleung1@gmail.com>
2023-10-17 01:18:08 -0400

    scrape info on all sections from single course page

--- webscraper.py
+++ webscraper.py
@@ -1,8 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 url = 'https://www.bu.edu/academics/cas/courses/cas-cs-101/'
 browser = webdriver.Chrome()
 browser.get(url) 
-element = browser.find_element(By.CLASS_NAME,'cf-hub-offerings')
-print(element.text)
+hubList = browser.find_element(By.CLASS_NAME,'cf-hub-offerings')
+courseList = browser.find_element(By.CLASS_NAME,'cf-course')
+# print(hubList.text)
+tableNum = 1
+while True:
+    try:
+        table = browser.find_element(By.XPATH,f'//*[@id="course-content"]/div[3]/table[{tableNum}]/tbody[1]/tr[2]')
+        print(table.text)
+        tableNum += 1
+    except NoSuchElementException:
+        break
+
+
+
+
