797a4af5b7e9d3f6afb724101fb9263bdaaa6e44
kaylieee <kaylieleung1@gmail.com>
2023-10-17 02:09:13 -0400

    get info on all courses on a course list page

--- webscraper.py
+++ webscraper.py
@@ -2,29 +2,40 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 
-url = 'https://www.bu.edu/academics/cas/courses/cas-cs-101/'
+startPage = 'https://www.bu.edu/academics/cas/courses/'
 browser = webdriver.Chrome()
-browser.get(url) 
-hubList = browser.find_element(By.CLASS_NAME,'cf-hub-offerings')
-courseList = browser.find_element(By.CLASS_NAME,'cf-course')
-# print(hubList.text)
-tableNum = 1
-fallSections = []
-springSections = []
+browser.get(startPage)
+linkNum = 1
+courseList = {}
 while True:
     try:
-        semester = browser.find_element(By.XPATH,f'//*[@id="course-content"]/div[3]/h4[{tableNum}]')
-        table = browser.find_element(By.XPATH,f'//*[@id="course-content"]/div[3]/table[{tableNum}]/tbody[1]/tr[2]')
-        if semester.text[0] == 'F':
-            fallSections.append(table.text)
-        else:
-            springSections.append(table.text)
-        tableNum += 1
+        url = browser.find_element(By.XPATH,f'//*[@id="post-6984"]/ul/li[{linkNum}]/a').get_attribute('href')
+        browser.get(url)
+        course = {}
+        try:
+            hubList = browser.find_element(By.CLASS_NAME,'cf-hub-offerings')
+            course["hub"] = hubList.text
+        except NoSuchElementException:
+            pass
+        tableNum = 1
+        fallSections = []
+        springSections = []
+        while True:
+            try:
+                semester = browser.find_element(By.XPATH,f'//*[@id="course-content"]/div[3]/h4[{tableNum}]')
+                table = browser.find_element(By.XPATH,f'//*[@id="course-content"]/div[3]/table[{tableNum}]/tbody[1]/tr[2]')
+                if semester.text[0] == 'F':
+                    fallSections.append(table.text)
+                else:
+                    springSections.append(table.text)
+                tableNum += 1
+            except NoSuchElementException:
+                break
+        course["fall"] = fallSections
+        course["spring"] = springSections
+        courseName = browser.find_element(By.XPATH,'//*[@id="col1"]/div/h2').text
+        courseList[courseName] = course
+        browser.get(startPage)
+        linkNum += 1
     except NoSuchElementException:
         break
-print(fallSections)
-print(springSections)
-
-
-
-
