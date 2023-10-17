58aceaa21c70d68fff08fdf9e43c2343b686e9e4
kaylieee <kaylieleung1@gmail.com>
2023-10-17 02:31:59 -0400

    changed loop so don't have to return to start page

--- webscraper.py
+++ webscraper.py
@@ -5,37 +5,33 @@ from selenium.common.exceptions import NoSuchElementException
 startPage = 'https://www.bu.edu/academics/cas/courses/'
 browser = webdriver.Chrome()
 browser.get(startPage)
-linkNum = 1
 courseList = {}
-while True:
+classes = browser.find_elements(By.XPATH,'//*[@id="post-6984"]/ul/li/a')
+urls = [c.get_attribute("href") for c in classes]
+for url in urls:
+    browser.get(url)
+    courseTitle = browser.find_element(By.XPATH,'//*[@id="col1"]/div/h2').text
+    course = {}
     try:
-        url = browser.find_element(By.XPATH,f'//*[@id="post-6984"]/ul/li[{linkNum}]/a').get_attribute('href')
-        browser.get(url)
-        course = {}
-        try:
-            hubList = browser.find_element(By.CLASS_NAME,'cf-hub-offerings')
-            course["hub"] = hubList.text
-        except NoSuchElementException:
-            pass
-        tableNum = 1
-        fallSections = []
-        springSections = []
-        while True:
-            try:
-                semester = browser.find_element(By.XPATH,f'//*[@id="course-content"]/div[3]/h4[{tableNum}]')
-                table = browser.find_element(By.XPATH,f'//*[@id="course-content"]/div[3]/table[{tableNum}]/tbody[1]/tr[2]')
-                if semester.text[0] == 'F':
-                    fallSections.append(table.text)
-                else:
-                    springSections.append(table.text)
-                tableNum += 1
-            except NoSuchElementException:
-                break
-        course["fall"] = fallSections
-        course["spring"] = springSections
-        courseName = browser.find_element(By.XPATH,'//*[@id="col1"]/div/h2').text
-        courseList[courseName] = course
-        browser.get(startPage)
-        linkNum += 1
+        hubList = browser.find_element(By.CLASS_NAME,'cf-hub-offerings')
+        course["hub"] = hubList.text
     except NoSuchElementException:
-        break
+        pass
+    tableNum = 1
+    fallSections = []
+    springSections = []
+    while True:
+        try:
+            semester = browser.find_element(By.XPATH,f'//*[@id="course-content"]/div[3]/h4[{tableNum}]')
+            table = browser.find_element(By.XPATH,f'//*[@id="course-content"]/div[3]/table[{tableNum}]/tbody[1]/tr[2]')
+            if semester.text[0] == 'F':
+                fallSections.append(table.text)
+            else:
+                springSections.append(table.text)
+            tableNum += 1
+        except NoSuchElementException:
+            break
+    course["fall"] = fallSections
+    course["spring"] = springSections
+    courseList[courseTitle] = course
+print(courseList)
